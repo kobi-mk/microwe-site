0cafc601ffe8affb72764cdd0f650c0e9a4c1353
kobi-mk <162877172+kobi-mk@users.noreply.github.com>
2024-04-22 16:14:49 +0530

    okok
    koko

--- src/components/Flex.js
+++ src/components/Flex.js
@@ -3,28 +3,47 @@ import FlexProduct from './FlexProduct';
 import axios from 'axios';
 
 const Flex = () => {
-    const [products, setProducts] = useState([]);
-    const [fetchError, setFetchError] = useState(null)
+    // const [products, setProducts] = useState([]);
+    // const [fetchError, setFetchError] = useState(null)
 
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const response = await axios.get('http://localhost:3500/flex'); // Here I used Axios to GET data from JSON server
-                // console.log(response);
-                const data = await response.data;
-                // console.log(data);
-                // setProducts(data); //whole data comes
-                setProducts(data.slice(0, 3)); // Use slice to get the first 3 items
-                // console.log(products);
-                setFetchError(null)
-            } catch (error) {
-                setFetchError(error.message)
-                // console.log(fetchError);
-            }
-        };
+    const products = [{
+        "id": "pd1",
+        "title": "Tasty Pizza",
+        "content": "It's hot, and succulent at the same time. It's truly a wonderful taste when you take first bite and the sound it makes.",
+        "img": "./images/pizza.jpeg"
+      },
+      {
+        "id": "pd2",
+        "title": "Cheesy Burger",
+        "content": "Crispy seasoned chicken breast, topped with melted cheese, piled onto soft rolls with onion, tomato and garlic mayo.",
+        "img": "./images/burger.jpeg"
+      },
+      {
+        "id": "pd3",
+        "title": "Spicy Shawarma",
+        "content": "Fiery Chicken Shawarma: slow-roasted for smoky flavor. Packed with a spicy kick and cooled by creamy tahini.",
+        "img": "./images/sawarma.jpeg"
+      }]
 
-        (async () => await fetchData())()
-    }, []);
+    // useEffect(() => {
+    //     const fetchData = async () => {
+    //         try {
+    //             const response = await axios.get('http://localhost:3500/flex'); // Here I used Axios to GET data from JSON server
+    //             // console.log(response);
+    //             const data = await response.data;
+    //             // console.log(data);
+    //             // setProducts(data); //whole data comes
+    //             setProducts(data.slice(0, 3)); // Use slice to get the first 3 items
+    //             // console.log(products);
+    //             setFetchError(null)
+    //         } catch (error) {
+    //             setFetchError(error.message)
+    //             // console.log(fetchError);
+    //         }
+    //     };
+
+    //     (async () => await fetchData())()
+    // }, []);
 
     return (
         <div className='container mx-auto bg-gray-400'>
@@ -32,7 +51,7 @@ const Flex = () => {
                 <div className="text-center">
                     <h1 className="text-4xl font-bold m-2">Our featured products.</h1>
                     {/* if error occurs display message to users */}
-                    {fetchError && <span className="text-red-500 font-bold text-6xl bg-gray-500 px-4 py-4 mt-8 flex justify-center">There is no feature products to show!!!</span>}
+                    {/* {fetchError && <span className="text-red-500 font-bold text-6xl bg-gray-500 px-4 py-4 mt-8 flex justify-center">There is no feature products to show!!!</span>} */}
                 </div>
             </div>
             <div className="flex flex-col md:flex-row justify-evenly space-x-4 px-7 pb-14 pt-7">
--- src/components/Grid.js
+++ src/components/Grid.js
@@ -2,35 +2,72 @@ import React, { useEffect, useState } from "react";
 import GridProduct from "./GridProduct";
 
 const Grid = () => {
-    const [products, setProducts] = useState([]);
-    const [fetchError, setFetchError] = useState(null)
+    // const [products, setProducts] = useState([]);
+    // const [fetchError, setFetchError] = useState(null)
 
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const response = await fetch('http://localhost:3500/grid'); // Here I used fetch API to GET data from JSON server
-                // console.log(response);
-                if (!response.ok) throw Error("Data not received")
-                const data = await response.json();
-                // console.log(data);
-                setProducts(data);
-                // console.log(products);
-                setFetchError(null)
-            } catch (error) {
-                setFetchError(error.message)
-                // console.log(error.message);
-            }
-        };
+    // useEffect(() => {
+    //     const fetchData = async () => {
+    //         try {
+    //             const response = await fetch('http://localhost:3500/grid'); // Here I used fetch API to GET data from JSON server
+    //             // console.log(response);
+    //             if (!response.ok) throw Error("Data not received")
+    //             const data = await response.json();
+    //             // console.log(data);
+    //             setProducts(data);
+    //             // console.log(products);
+    //             setFetchError(null)
+    //         } catch (error) {
+    //             setFetchError(error.message)
+    //             // console.log(error.message);
+    //         }
+    //     };
 
-        (async () => await fetchData())()
-    }, []);
+    //     (async () => await fetchData())()
+    // }, []);
+
+    const products = [{
+        "id": "pd1",
+        "title": "Healthy Salad",
+        "content": "Crispy seasoned chicken breast, topped with melted cheese, piled onto soft rolls with onion, tomato and garlic mayo.",
+        "img": "./images/salad.jpeg"
+      },
+      {
+        "id": "pd2",
+        "title": "Microwe Fried Chicken",
+        "content": "Crispy seasoned chicken breast, topped with melted cheese, piled onto soft rolls with onion, tomato and garlic mayo.",
+        "img": "./images/mfc.jpeg"
+      },
+      {
+        "id": "pd3",
+        "title": "Creamy twist",
+        "content": "Crispy seasoned chicken breast, topped with melted cheese, piled onto soft rolls with onion, tomato and garlic mayo.",
+        "img": "./images/twist.jpeg"
+      },
+      {
+        "id": "pd4",
+        "title": "Crunchy Fries",
+        "content": "Crispy seasoned chicken breast, topped with melted cheese, piled onto soft rolls with onion, tomato and garlic mayo.",
+        "img": "./images/frenchfries.jpeg"
+      },
+      {
+        "id": "pd5",
+        "title": "Yummy BBQ",
+        "content": "Crispy seasoned chicken breast, topped with melted cheese, piled onto soft rolls with onion, tomato and garlic mayo.",
+        "img": "./images/bbq.jpeg"
+      },
+      {
+        "id": "pd6",
+        "title": "Tasty Noodles",
+        "content": "Crispy seasoned chicken breast, topped with melted cheese, piled onto soft rolls with onion, tomato and garlic mayo.",
+        "img": "./images/noodles.jpeg"
+      }]
 
     return (
         <div className="container mx-auto bg-gray-400 pt-4 -m-1">
             <div className="text-center">
                 <h1 className="text-4xl font-bold m-2 pb-6">What do we have?</h1>
                 {/* if error occurs display message to users */}
-                {fetchError && <span className="text-red-500 font-bold text-6xl bg-gray-500 px-4 py-4 mt-8 flex justify-center">There is no feature products to show!!!</span>}
+                {/* {fetchError && <span className="text-red-500 font-bold text-6xl bg-gray-500 px-4 py-4 mt-8 flex justify-center">There is no feature products to show!!!</span>} */}
             </div>
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-y-8 xl:gap-y-2 2xl:gap-1 px-5 py-8 ">
                 {products.map((product) => (
